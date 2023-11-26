2eb9042dc073b495e9ead31ed2db4d1bdc87fe90
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-26 15:10:13 +0100

    added duration increment\decrement

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { useState, memo } from "react";
+import { useState, memo, useEffect } from "react";
 import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
@@ -6,8 +6,15 @@ function Calculator({ workouts, allowSound }) {
   const [sets, setSets] = useState(3);
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
+  const [duration, setDuration] = useState(0);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  useEffect(
+    function () {
+      setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+    },
+    [number, sets, speed, durationBreak]
+  );
+
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
@@ -16,6 +23,14 @@ function Calculator({ workouts, allowSound }) {
     const sound = new Audio(clickSound);
     sound.play();
   };
+
+  function handleIncrement() {
+    setDuration((duration) => Math.floor(duration) + 1);
+  }
+
+  function handleDecrement() {
+    setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
+  }
 
   return (
     <>
@@ -77,12 +92,12 @@ function Calculator({ workouts, allowSound }) {
         </div>{" "}
       </form>{" "}
       <section>
-        <button onClick={() => {}}> – </button>{" "}
+        <button onClick={handleDecrement}> – </button>{" "}
         <p>
           {" "}
           {mins < 10 && "0"} {mins} : {seconds < 10 && "0"} {seconds}{" "}
         </p>{" "}
-        <button onClick={() => {}}> + </button>{" "}
+        <button onClick={handleIncrement}> + </button>{" "}
       </section>{" "}
     </>
   );
